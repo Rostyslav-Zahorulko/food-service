6442b44362bea154b3248eaed097c42117dff321
Rostyslav Zahorulko <rostyslav.zagorulko@gmail.com>
2020-12-05 11:33:30 +0200

    Змiнює файли

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import './styles.css';
+import template from './templates/gallery-item.hbs';
+import foods from './menu.json';
+
+const markup = template(foods);
+
+const galleryRef = document.querySelector('.js-menu');
+
+galleryRef.insertAdjacentHTML('beforeend', markup);
